5fbf0761d2b97a45b5c61d3fd7c52c31fc5c3301
Greg Weng <snowmantw@gmail.com>
2017-08-23 21:25:03 +0900

    Add 1 more condition in Summary panel; Next: add controllers

--- src/Summary/component/ConditionDisplay.js
+++ src/Summary/component/ConditionDisplay.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './ConditionDisplay.css';
 
 const ConditionDisplay = (props) => {
+   // in K
+  const tempC = Math.ceil(props.conditions.weather.main.temp - 273.15)
   return (
     <div className="ConditionDisplay">
-      { `Air Quality: ${props.condition.data.aqi}` }
+      <div className="ConditionLine"> { `Air Quality: ${props.conditions.air.data.aqi}` } </div>
+      <div className="ConditionLine"> { `Temperature: ${tempC}` } </div>
     </div>
   );
 };
--- src/Summary/effect/Initializing.js
+++ src/Summary/effect/Initializing.js
@@ -1,11 +1,23 @@
 
 export default (io, transformed) => {
-  const token = '1ac78dd2e6a67dec2a9bb88d2a2bfe39458bd157'; 
-  const apicall =
-    `https://api.waqi.info/feed/geo:${transformed.latitude};${transformed.longitude}/?token=${token}`;
-  return io.fetch(apicall)
+  const airCondToken = '1ac78dd2e6a67dec2a9bb88d2a2bfe39458bd157'; 
+  const airCondEndpoint =
+    `https://api.waqi.info/feed/geo:${transformed.latitude};${transformed.longitude}/?token=${airCondToken}`;
+
+  const weatherToken = '060b0412f55ae83626ef7ff45c81d38f';
+  const weatherCondEndpoint =
+    `https://api.openweathermap.org/data/2.5/weather?lat=${transformed.latitude}&lon=${transformed.longitude}&APPID=${weatherToken}`;
+
+  return io.fetch(airCondEndpoint)
   .then((body) => { return body.json(); })
-  .then((condition) => {
-    return { condition };  // Now it is an air quality data result.
+  .then((airCond) => {
+    return io.fetch(weatherCondEndpoint).then((body) => {
+      return body.json();
+    }).then((weatherCond) => {
+      return { weather: weatherCond, air: airCond };
+    });
+  })
+  .then((conditions) => {
+    return { conditions };  // Now it is an air quality data result.
   });
 };
